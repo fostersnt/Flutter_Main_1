00af83ff19db6efff873c9a4b5da72e500f2ae80
= <=>
2023-12-10 11:31:38 +0000

    Added properties and constructor for the student model

---
+++ lib/models/student_model.dart
@@ -0,0 +1,7 @@
+class StudentModel {
+  String name = '';
+  int age = 0;
+  String currentClass = '';
+  StudentModel(
+      {required this.name, required this.age, required this.currentClass});
+}
